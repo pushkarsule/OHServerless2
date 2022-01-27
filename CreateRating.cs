71f4df70fd12cc43bba76c929913eca9dcaf7d33
John Doe <john.doe@contoso.com>
2022-01-27 16:41:13 -0600

    fix issues

--- CreateRating.cs
+++ CreateRating.cs
@@ -7,42 +7,47 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Company.Function
 {
-    public class Person
-{
-    public Person()
+        public class Todo
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
 
-    }
-    public Person(string name)
+    public static class TodoApiInMemory
     {
-        Name = name;
+        static List<Todo> items = new List<Todo>();
+
+        //...
     }
-//comment
-    public string Name { get; set; }
-}
+    public class TodoCreateModel
+    {
+        public string TaskDescription { get; set; }
+    }
+
     public static class CreateRating
     {
         [FunctionName("CreateRating")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] Person req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Name;//.Query["name"];
+            
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            
+    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+    var input = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+    var todo = new Todo() { TaskDescription = input.TaskDescription };
+    //items.Add(todo);
+    return new OkObjectResult(todo);
         }
     }
 }
